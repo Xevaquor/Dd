5ee9a8ad19cb5ce168d8c216b2794efbe9db41ca
Xevaquor <xevaquor@hotmail.com>
2013-12-15 23:10:45 +0100

    Zmiana koncepcji:
    Żadnej bazy danycj
    D7
    ---
    Rekord agenta

---
+++ TOperativeUnit.pas
@@ -0,0 +1,16 @@
+unit TOperativeUnit;
+
+interface
+
+type
+	TOperative = record
+		FirstName : ShortString;
+		LastName : ShortString;
+		DateOfBirth : TDateTime;
+		BirthPlace : ShortString;
+		ImagePath : ShortString;
+	end;
+
+implementation
+
+end.
